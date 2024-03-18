91f60cd2e8b1327d4fad726aaa7a93a98523a0d3
Heather <heather.hellowoo@gmail.com>
2024-03-18 16:50:50 +0900

    request 테스트

--- src/main/java/boot/servlet/ServletApplication.java
+++ src/main/java/boot/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package boot.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan	//서블릿 자동등록
 @SpringBootApplication
 public class ServletApplication {
 
---
+++ src/main/java/boot/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,42 @@
+package boot.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        printStartLine(req);
+        printHeaders(req);
+    }
+    private void printStartLine(HttpServletRequest request) {
+        System.out.println("--- REQUEST-LINE - start ---");
+        System.out.println("request.getMethod() = " + request.getMethod()); //GET
+        System.out.println("request.getProtocol() = " + request.getProtocol()); //HTTP/1.1
+        System.out.println("request.getScheme() = " + request.getScheme()); //http
+        // http://localhost:8080/request-header
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+        // /request-header
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+        //username=hi
+        System.out.println("request.getQueryString() = " + request.getQueryString());
+        System.out.println("request.isSecure() = " + request.isSecure()); //https 사용유무
+        System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+    }
+
+    private void printHeaders(HttpServletRequest request) {
+        System.out.println("--- HEADERS-LINE - start ---");
+
+        request.getHeaderNames().asIterator().forEachRemaining(headerName -> System.out.println(headerName + " : " + headerName));
+
+        System.out.println("--- HEADERS-LINE - end ---");
+        System.out.println();
+    }
+}
