7490836c00c488dbdc0476d70be61c63cdadcacb
Heather <heather.hellowoo@gmail.com>
2024-03-25 16:15:43 +0900

    4차 mvc

---
+++ src/main/java/boot/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -0,0 +1,20 @@
+package boot.servlet.web.frontcontroller.v4.controller;
+
+import boot.servlet.domain.member.Member;
+import boot.servlet.domain.member.MemberRepository;
+import boot.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV4 implements ControllerV4 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        List<Member> members = memberRepository.findAll();
+        model.put("members", members);
+        return "members";
+    }
+}
